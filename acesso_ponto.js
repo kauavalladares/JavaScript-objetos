d6f157982c197a2cbaa749beec10bacb5ea98763
Kauavalladares <kaua.valldares@gmail.com>
2023-06-06 15:40:00 -0300

    leve modificação no exercicio.

--- acesso_ponto.js
+++ acesso_ponto.js
@@ -6,7 +6,11 @@ const cliente = {
 };
 
 console.log(
-  `O nome do cliente é ${cliente.nome} e essa pessoa tem ${cliente.idade} anos.`
+  `O nome do cliente é ${cliente["nome"]} e essa pessoa tem ${cliente["idade"]} anos.`
 );
 
-console.log(`Os 3 primeiros digitos do CPF são ${cliente.cpf.substring(0, 3)}`);
+const chaves = ["nome", "idade", "cpf", "email", "altura"];
+
+chaves.forEach((chave) => {
+  console.log(`A chave ${chave} tem valor ${cliente[chave]}`);
+});
